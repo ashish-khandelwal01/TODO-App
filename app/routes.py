9056fcbdfd894e16131ffa88db81faab1f1abd25
Ashish Khandelwal <ashishkhandelwal01@hotmail.com>
2025-03-16 11:37:20 -0400

    Initial design of the app

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,32 @@
-from flask import current_app as app
+from flask import current_app as app, render_template, request, redirect, url_for
+from .models import db, Task
 
 @app.route('/')
-def home():
-    return 'Hello, Flask!'
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form.get('title')
+    if title:
+        new_task = Task(title=title)
+        db.session.add(new_task)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:task_id>')
+def complete(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        task.completed = not task.completed
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+    return redirect(url_for('index'))
