931056df379f80891adaac694d2d79fc1901990b
Ashish Khandelwal <ashishkhandelwal01@hotmail.com>
2025-03-16 22:50:27 -0400

    Registration page complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models import User  # Move import here to avoid circular import
+    return User.query.get(int(user_id))
 
 def create_app():
     app = Flask(__name__)
-    app.config['SdQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'your_secret_key'
 
     db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
 
     with app.app_context():
         from . import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,48 @@
-from flask import current_app as app, render_template, request, redirect, url_for
-from .models import db, Task
+from flask import current_app as app, render_template, request, redirect, url_for, jsonify
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db
+from app.models import User, Task
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+        if password != confirm_password:
+            flash('Passwords do not match', 'danger')
+            return redirect(url_for('register'))
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
 
 @app.route('/')
+@login_required
 def index():
     tasks = Task.query.all()
     return render_template('index.html', tasks=tasks)
@@ -30,3 +71,12 @@ def delete(task_id):
         db.session.delete(task)
         db.session.commit()
     return redirect(url_for('index'))
+
+
+@app.route('/check_username', methods=['POST'])
+def check_username():
+    existing_usernames = [user.username for user in User.query.all()]
+    username = request.form.get('username')
+    if username in existing_usernames:
+        return jsonify({'exists': True})
+    return jsonify({'exists': False})
